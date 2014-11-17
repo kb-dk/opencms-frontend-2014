d04cf48706ed4a4f5bba822c55e98f8c15192191
Hasse Feldthaus <hafe@kb.dk>
2014-11-17 17:30:10 +0100

    Bugfix/Refactor: Top navbar works with alertbox now.
    Top navigation inside an outer container kb-navbar-container with overflow:hidden.
    kb-navbar-container can have either .mini (small devices) or .micro (scrolling) depending on desired height.
    Better use og nav html5 elements.
    Removed transition, and keeping horizontal menu items at same x pos.
    Still lacks some code cleanup especially in the navbar.less file

--- js/kbListeners.js
+++ js/kbListeners.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
         DESKTOPSTR = 'desktop',
         $body = $('body'),
         $topnavigation = $('.topnavigation'),
+        $kbNavbarContainer = $('.kb-navbar-container'),
         MOBILE = 752, // magic-number 752 = 768 - 2 * 8px (window-borders?)
         TABLET = 1008, // magic-number 1008 = 1024 - 2 * 8px
         initialWidth = $(window).innerWidth(); // FIXME: switch to outerWidth instead?
@@ -22,14 +23,35 @@ $(document).ready(function() {
     };
 
     var ajustHeaderHeight = function () {
-        var scrollTop = $(window).scrollTop();
-        if ((kbModus === DESKTOPSTR) && (scrollTop <= 100)) {
-            if ($topnavigation.hasClass('minified')) {
-                $topnavigation.removeClass('minified');
+        var scrollTop = $(window).scrollTop(),
+            $body = $('body');
+        if (kbModus === DESKTOPSTR) {
+            if (scrollTop <= 100) {
+                if ($kbNavbarContainer.hasClass('micro')) {
+                    $kbNavbarContainer.removeClass('micro');
+                }
+            } else {
+                if (!$kbNavbarContainer.hasClass('micro')) {
+                    $kbNavbarContainer.addClass('micro');
+                }
             }
         } else {
-            if (!$topnavigation.hasClass('minified')) {
-                $topnavigation.addClass('minified');
+            if (!$kbNavbarContainer.hasClass('mini')) {
+                $kbNavbarContainer.addClass('mini');
+            }
+        }
+        // ajust top margin to fit alert + header
+        var alertHeight = $('section.alert', $topnavigation).outerHeight(),
+            allNavHeaderHeight =  alertHeight + $('section.kb-navbar-container', $topnavigation).outerHeight();
+        if ($body.hasClass('desktop')) { // body.desktop
+            if (allNavHeaderHeight !== $('body').data('margin-top')) {
+                $body.css('margin-top', allNavHeaderHeight);
+                $body.data('margin-top', allNavHeaderHeight);
+            }
+        } else { // body.tablet or body.mobile
+            if ($('body').data('margin-top') !== 0) {
+                $body.css('margin-top', 0);
+                $body.data('margin-top', 0);
             }
         }
     }
@@ -45,6 +67,7 @@ $(document).ready(function() {
             }
         } else if (innerWidth < TABLET) {
             if (kbModus !== TABLETSTR) {
+                $kbNavbarContainer.removeClass('micro').addClass('mini');
                 setModus(TABLETSTR);
                 ajustHeaderHeight();
             }
@@ -53,6 +76,7 @@ $(document).ready(function() {
                 if (kbModus === TABLETSTR) { // if we come from the tablet view, close menu (if it is present).
                     $body.removeClass('showMenu');
                 }
+                $kbNavbarContainer.removeClass('mini');
                 setModus(DESKTOPSTR);
                 ajustHeaderHeight();
             }
@@ -60,6 +84,7 @@ $(document).ready(function() {
     });
 
     // initialize the header size
+    $('body').data('margin-top', 120); // initial margin-top : 120; // we keep the margin-top in data, to avoid having to set a new margin-top unless it is necessary
     ajustHeaderHeight();
 
     //initialize listeners that toggles chevrons
