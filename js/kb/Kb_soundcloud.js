1981ecf08baea51e1a26803e8e800cfeffcb7f63
Hasse Feldthaus <hafe@kb.dk>
2015-09-16 13:14:30 +0200

    Added playlist fetched from soundcloud.

---
+++ js/kb/Kb_soundcloud.js
@@ -0,0 +1,44 @@
+/*global jQuery, SC*/
+var kb_soundcloud = (function (window, $, undefined) {
+    var LANGUAGE = 'da'; // Change this to either 'da' or 'en'
+
+    var Kb_soundcloud = function () { };
+
+    Kb_soundcloud.prototype = {
+        CLIENTNAME: 'cortexcowboy',
+        CLIENTID: 'addc49835216955834db7171a0a41411',
+        PLAYLIST: '//soundcloud.com/cortexcowboy/sets/kb-playlist',
+        PLAYLISTID: '141435127'
+    };
+
+    return new Kb_soundcloud();
+
+})(window, jQuery);
+
+// Initializing SC
+SC.initialize({
+  client_id: kb_soundcloud.CLIENTID
+});
+
+SC.get('/users/' + kb_soundcloud.CLIENTNAME + '/playlists/' + kb_soundcloud.PLAYLISTID, function (playlist, error) {
+    if (!error && playlist && playlist.tracks && playlist.tracks.length) { // No errors, threre is a playlist, and it has tracks - do the rendering
+        var sounds = playlist.tracks.map(function (track) {
+                return  '<div class="col-xs-6 col-sm-4 col-md-3">' +
+                            '<a href="soundArticle.html?pid=' + track.id + '">' +
+                                '<article class="comp video" style="background-image: url(' + track.artwork_url + ')">' +
+                                    '<div class="caption">' +
+                                        '<span class="glyphicon glyphicon-play-circle pull-right"></span>' +
+                                        '<h3>' + track.title + '</h3>' +
+                                    '</div>' +
+                                '</article>' +
+                            '</a>' +
+                        '</div>';
+            });
+        $('#soundlist').append('<div class="container"><div class="row"><header class="text-center"><h2>Sounds</h2><p class="lead"></p></header></div></div>');
+        $('#soundlist .container .row').append(sounds);
+    } else {
+        if ('undefined' !== typeof console) {
+            console.warn('Error fetching soundcloud playlist.');
+        }
+    }
+});
