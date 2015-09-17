82889dca27d1aa670952a209de5fba2e1058ac8f
Hasse Feldthaus <hafe@kb.dk>
2015-09-17 09:10:37 +0200

    Bundling youtube and soundcloud objects for pages that needs both.
    Note that this is done in a seperat gulp step, since I didn't succeed in
    working on production files in one step (probably because they are
    still streaming when I try to concat them?)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-/* global require, console */
+/* global require */
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 var del = require('del');
 
 gulp.task('default', ['production'], function () {});
@@ -20,6 +21,20 @@ gulp.task('production', ['clean'], function (cb) {
     .pipe(rename(function (path) {
         path.basename += '_min';
     }))
+    .pipe(gulp.dest('production'));
+
+    gutil.log('Create a bundle of ', gutil.colors.cyan('Kb_youtube_min.js'), 'and', gutil.colors.cyan('Kb_soundcloud_min.js'), 'by typing', gutil.colors.green('gulp bundle'));
+
+    if (cb) {
+        cb();
+    }
+});
+
+gulp.task('bundle', [], function (cb) {
+    gutil.log('Doing', gutil.colors.cyan('postprod'), 'build');
+    gutil.log('Concatening youtube and soundcloud minified scripts ...');
+    gulp.src(['production/Kb_youtube_min.js', 'production/Kb_soundcloud_min.js'])
+    .pipe(concat('Kb_youtubeSoundcĺoudBundle.js'))
     .pipe(gulp.dest('production'));
 
     if (cb) {
