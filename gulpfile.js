925ea24a8248cdf821e1d984056bcf496df0f1e6
Hasse Feldthaus <hafe@kb.dk>
2015-10-23 20:05:10 +0200

    Added a development gulp target for noncompressed js files.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var del = require('del');
 gulp.task('default', ['production'], function () {});
 
 gulp.task('clean', function (cb) {
-    del(['production'], cb);
+    del(['production', 'development'], cb);
 });
 
 gulp.task('production', ['clean'], function (cb) {
@@ -30,11 +30,22 @@ gulp.task('production', ['clean'], function (cb) {
     }
 });
 
+gulp.task('development', [], function (cb) {
+    gutil.log('Building a ', gutil.colors.cyan('development'), 'build');
+    gutil.log('copying youtube and soundcloud scripts');
+    gulp.src(['js/kb/Kb_youtube.js', 'js/kb/Kb_soundcloud.js'])
+    .pipe(gulp.dest('development'));
+    gutil.log('Concatenating mediachannel, youtube and soundcloud script in a bundle');
+    gulp.src(['js/kb/Kb_mediaChannel.js','js/kb/Kb_youtube.js','js/kb/Kb_soundcloud.js'])
+    .pipe(concat('Kb_youtubeSoundcloudBundle_full.js'))
+    .pipe(gulp.dest('development'));
+});
+
 gulp.task('bundle', [], function (cb) {
     gutil.log('Doing', gutil.colors.cyan('postprod'), 'build');
     gutil.log('Concatening youtube and soundcloud minified scripts ...');
     gulp.src(['production/Kb_mediaChannel_min.js', 'production/Kb_youtube_min.js', 'production/Kb_soundcloud_min.js'])
-    .pipe(concat('Kb_youtubeSoundcĺoudBundle.js'))
+    .pipe(concat('Kb_youtubeSoundcloudBundle.js'))
     .pipe(gulp.dest('production'));
 
     if (cb) {
